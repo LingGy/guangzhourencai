65181f1f897697dfb5f1e6d74a21829edc702323
linguiyuan <13428945556@163.com>
2018-07-18 14:06:44 +0800

    20180718
    关系列表中新增可选择关系中的人才添加成一个小组功能

--- src/components/Application/relationInfo.vue
+++ src/components/Application/relationInfo.vue
@@ -1,84 +1,90 @@
 <template>
-<div id="relationInfo">
-  <p class="position"><i class='iconfont'>&#xe8e6;</i>您现在的位置 : 数据应用 > 关系分析 > 关系详情</p>
-  <!--<table class="college"></table>-->
-  <!--<table class="institution"></table>-->
-  <!--<table class="industry"></table>-->
-  <!--<table class="nationality"></table>-->
-  <!--<table class="project"></table>-->
-  <button class="return" @click='goBack()'>返回</button>
-  <table>
-    <caption>校友关系</caption>
-    <thead>
-    <tr>
-      <th>序号</th>
-      <th>中文名</th>
-      <th>英文名</th>
-      <th>人才ID</th>
-      <th>操作</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="(list,index) in college" :key="index">
-      <td>{{index+1}}</td>
-      <td>{{list.name}}</td>
-      <td>{{list.EnglishName}}</td>
-      <td>{{list.Id}}</td>
-      <td>
-        <button class="info" @click="toInfo(list.Id)">人才详情</button>
-      </td>
-    </tr>
-    </tbody>
-  </table>
-  <table>
-    <caption>同事关系</caption>
-    <thead>
-    <tr>
-      <th>序号</th>
-      <th>中文名</th>
-      <th>英文名</th>
-      <th>人才ID</th>
-      <th>操作</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="(list,index) in institution" :key="index">
-      <td>{{index+1}}</td>
-      <td>{{list.name}}</td>
-      <td>{{list.EnglishName}}</td>
-      <td>{{list.Id}}</td>
-      <td>
-        <button class="info" @click="toInfo(list.Id)">人才详情</button>
-      </td>
-    </tr>
-    </tbody>
-  </table>
-  <table>
-    <caption>同行关系</caption>
-    <thead>
-    <tr>
-      <th>序号</th>
-      <th>中文名</th>
-      <th>英文名</th>
-      <th>人才ID</th>
-      <th>操作</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="(list,index) in industry" :key="index">
-      <td>{{index+1}}</td>
-      <td>{{list.name}}</td>
-      <td>{{list.EnglishName}}</td>
-      <td>{{list.Id}}</td>
-      <td>
-        <button class="info" @click="toInfo(list.Id)">人才详情</button>
-      </td>
-    </tr>
-    </tbody>
-  </table>
-  <table>
-    <caption>同国籍关系</caption>
-    <thead>
+  <div id="relationInfo">
+    <p class="position"><i class='iconfont'>&#xe8e6;</i>您现在的位置 : 数据应用 > 关系分析 > 关系详情</p>
+    <button class="return" @click='goBack()'>返回</button>
+    <!--校友-->
+    <el-checkbox-group v-model="checkedIdArr" @change="handleCheckedCitiesChange">
+      <table>
+        <caption>校友关系</caption>
+        <thead>
+        <tr>
+          <th>序号</th>
+          <th>中文名</th>
+          <th>英文名</th>
+          <th>人才ID</th>
+          <th>操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(list,index) in college" :key="index">
+          <td>
+            <el-checkbox :label="list.Id">{{index+1}}</el-checkbox>
+          </td>
+          <td>{{list.name}}</td>
+          <td>{{list.EnglishName}}</td>
+          <td>{{list.Id}}</td>
+          <td>
+            <button class="info" @click="toInfo(list.Id)">人才详情</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    <!--同事-->
+      <table>
+        <caption>同事关系</caption>
+        <thead>
+        <tr>
+          <th>序号</th>
+          <th>中文名</th>
+          <th>英文名</th>
+          <th>人才ID</th>
+          <th>操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(list,index) in institution" :key="index">
+          <td>
+            <el-checkbox :label="list.Id">{{index+1}}</el-checkbox>
+          </td>
+          <td>{{list.name}}</td>
+          <td>{{list.EnglishName}}</td>
+          <td>{{list.Id}}</td>
+          <td>
+            <button class="info" @click="toInfo(list.Id)">人才详情</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    <!--同行-->
+      <table>
+        <caption>同行关系</caption>
+        <thead>
+        <tr>
+          <th>序号</th>
+          <th>中文名</th>
+          <th>英文名</th>
+          <th>人才ID</th>
+          <th>操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(list,index) in industry" :key="index">
+          <td>
+            <el-checkbox :label="list.Id">{{index+1}}</el-checkbox>
+          </td>
+          <td>{{list.name}}</td>
+          <td>{{list.EnglishName}}</td>
+          <td>{{list.Id}}</td>
+          <td>
+            <button class="info" @click="toInfo(list.Id)">人才详情</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    <!--同国籍-->
+      <table>
+      <caption>同国籍关系</caption>
+      <thead>
       <tr>
         <th>序号</th>
         <th>中文名</th>
@@ -86,10 +92,10 @@
         <th>人才ID</th>
         <th>操作</th>
       </tr>
-    </thead>
-    <tbody>
+      </thead>
+      <tbody>
       <tr v-for="(list,index) in nationality" :key="index">
-        <td>{{index+1}}</td>
+        <td><el-checkbox :label="list.Id">{{index+1}}</el-checkbox></td>
         <td>{{list.name}}</td>
         <td>{{list.EnglishName}}</td>
         <td>{{list.Id}}</td>
@@ -97,44 +103,61 @@
           <button class="info" @click="toInfo(list.Id)">人才详情</button>
         </td>
       </tr>
-    </tbody>
-  </table>
-  <table>
-    <caption>同项目关系</caption>
-    <thead>
-    <tr>
-      <th>序号</th>
-      <th>中文名</th>
-      <th>英文名</th>
-      <th>人才ID</th>
-      <th>操作</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="(list,index) in project" :key="index">
-      <td>{{index+1}}</td>
-      <td>{{list.name}}</td>
-      <td>{{list.EnglishName}}</td>
-      <td>{{list.Id}}</td>
-      <td>
-        <button class="info" @click="toInfo(list.Id)">人才详情</button>
-      </td>
-    </tr>
-    </tbody>
-  </table>
-</div>
+      </tbody>
+    </table>
+    <!--同项目-->
+    <table>
+      <caption>同项目关系</caption>
+      <thead>
+      <tr>
+        <th>序号</th>
+        <th>中文名</th>
+        <th>英文名</th>
+        <th>人才ID</th>
+        <th>操作</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(list,index) in project" :key="index">
+        <td><el-checkbox :label="list.Id">{{index+1}}</el-checkbox></td>
+        <td>{{list.name}}</td>
+        <td>{{list.EnglishName}}</td>
+        <td>{{list.Id}}</td>
+        <td>
+          <button class="info" @click="toInfo(list.Id)">人才详情</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    </el-checkbox-group>
+
+    <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+    <div class="end_box">
+      <div class="end">
+        <div class="name_box">
+          <p class="name">组名&nbsp;:&nbsp;</p>
+          <input type="text" class="int_name" placeholder="请输入组名" v-model='teamName' maxlength='24'>
+        </div>
+        <button class="saveTeamName" @click='saveTeamName(checkedIdArr,teamName)'>保存</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default {
-    name:"relationInfo",
+    name: "relationInfo",
     data: function () {
       return {
-        college:[],
-        institution:[],
-        industry:[],
-        nationality:[],
-        project:[]
+        college: [],
+        institution: [],
+        industry: [],
+        nationality: [],
+        project: [],
+        checkedIdArr: [],
+        teamName:null,
+        checkAll: false,
+        isIndeterminate: true,
       }
     },
     created: function () {
@@ -144,43 +167,49 @@
       this.getNationality();
       this.getProject();
     },
-    methods:{
+    methods: {
       //获取数据
       getNewDatas: function (relation) {
         let vm = this;
         let userid = sessionStorage.getItem("relationUserId");
         vm.$axios({
-            method:'post',
-            url:window.$g_url.ApiUrl + '/relationdetail?userid=' + userid +'&type='+relation,
+          method: 'post',
+          url: window.$g_url.ApiUrl + '/relationdetail?userid=' + userid + '&type=' + relation,
         })
-           .then(function(res){
-             let resdatas = res.data;
-             if(resdatas.code == 0){
-               if(resdatas.result){
-                 if(resdatas.result.length > 8){
+          .then(function (res) {
+            let resdatas = res.data;
+            if (resdatas.code == 0) {
+              if (resdatas.result) {
+                if (resdatas.result.length > 8) {
 
-                 }
-                 switch (relation) {
-                   case 'college':vm.college = resdatas.result;
+                }
+                switch (relation) {
+                  case 'college':
+                    vm.college = resdatas.result;
                     break;
-                   case 'institution':vm.institution = resdatas.result;
-                     break;
-                   case 'industry':vm.industry = resdatas.result;
-                     break;
-                   case 'nationality':vm.nationality = resdatas.result;
-                     break;
-                   case 'project':vm.project = resdatas.result;
-                     break;
-                   default:false;
-                 }
-               }
-             }else {
-               vm.$message.error(resdatas.message);
-             }
-           })
-           .catch(function(err){
-             console.log(err);
-           });
+                  case 'institution':
+                    vm.institution = resdatas.result;
+                    break;
+                  case 'industry':
+                    vm.industry = resdatas.result;
+                    break;
+                  case 'nationality':
+                    vm.nationality = resdatas.result;
+                    break;
+                  case 'project':
+                    vm.project = resdatas.result;
+                    break;
+                  default:
+                    false;
+                }
+              }
+            } else {
+              vm.$message.error(resdatas.message);
+            }
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
       },
       //获取校友关系数据
       getCollege: function () {
@@ -210,66 +239,146 @@
       //返回
       goBack: function () {
         this.$router.go(-1);
-      }
+      },
+      //全选
+      handleCheckAllChange(val) {
+        let leng = this.college.length + this.institution.length + this.industry.length + this.nationality.length + this.project.length
+        let arr = (((this.college.concat(this.institution)).concat(this.industry)).concat(this.nationality)).concat(this.project);
+        let idArr = [];
+        for (let i = 0, len = arr.length; i < len; i++) {
+          idArr.push(arr[i].Id);
+        }
+        this.checkedIdArr = val ? idArr : [];
+        this.isIndeterminate1 = false;
+      },
+      //多选
+      handleCheckedCitiesChange(value) {
+        let checkedCount = value.length;
+        console.log(value);
+        this.checkAll1 = checkedCount === this.checkedIdArr.length;
+        this.isIndeterminate1 = checkedCount > 0 && checkedCount < this.checkedIdArr.length;
+      },
+      //保存小组
+      saveTeamName: function (arr, teamName) {
+        let vm = this;
+        if (arr.length < 1) {
+          vm.$message.warning("请选择一项或多项添加到组");
+          return false;
+        }
+        vm.$axios({
+          method: 'post',
+          url: window.$g_url.ApiUrl + '/setgroupname?name=' + teamName,
+          data: JSON.stringify(arr)
+        })
+          .then(function (res) {
+            let resdata = res.data;
+            if (resdata.code == 0) {
+              vm.$message.success("保存小组成功!");
+            } else {
+              vm.$message.warning(resdata.message);
+            }
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      },
     }
   }
 </script>
 
 <style lang="scss" type="text/scss" scoped>
-#relationInfo{
-  .return{
-    display: block;
-    width: 50px;
-    height: 24px;
-    color: #ffffff;
-    background-color: #169bd8;
-    text-align: center;
-    &:hover{
-      background-color: #6ecffa;
+  #relationInfo {
+    .return {
+      display: block;
+      width: 50px;
+      height: 24px;
+      color: #ffffff;
+      background-color: #169bd8;
+      text-align: center;
+      &:hover {
+        background-color: #6ecffa;
+      }
     }
-  }
-  table,table tr th, table tr td {
-    border:1px solid #cccccc;
-  }
-  table{
-    min-width: 800px;
-    text-align: center;
-    border-collapse: collapse;
-    margin-top: 35px;
-    caption{
-      font-size: 16px;
-      height: 30px;
-      line-height: 30px;
-      font-weight: 600;
+    table, table tr th, table tr td {
+      border: 1px solid #cccccc;
     }
-    thead{
-      background-color: #f1f8ff;
-      font-size: 16px;
-      height: 30px;
-      line-height: 30px;
-      tr{
-        th{
-          font-weight: 400;
+    table {
+      min-width: 800px;
+      text-align: center;
+      border-collapse: collapse;
+      margin-top: 35px;
+      caption {
+        font-size: 16px;
+        height: 30px;
+        line-height: 30px;
+        font-weight: 600;
+      }
+      thead {
+        background-color: #f1f8ff;
+        font-size: 16px;
+        height: 30px;
+        line-height: 30px;
+        tr {
+          th {
+            font-weight: 400;
+          }
+        }
+      }
+      tbody {
+        font-size: 14px;
+        height: 30px;
+        line-height: 30px;
+        color: #454545;
+        tr {
+          &:hover {
+            background-color: rgba(204, 204, 204, 0.2);
+          }
+        }
+        .info {
+          color: #4ac0f6;
+          background-color: transparent;
+          text-decoration: underline;
         }
       }
     }
-    tbody{
-      font-size: 14px;
-      height: 30px;
-      line-height: 30px;
-      color: #454545;
-      tr{
-        &:hover{
-          background-color: rgba(204,204,204,0.2);
+    .end_box {
+      width: 800px;
+      .end {
+        width: 266px;
+        margin: 0 auto;
+        /*margin-top: 42px;*/
+        .name_box {
+          float: left;
+          .name {
+            height: 22px;
+            line-height: 22px;
+            font-size: 16px;
+            color: #454545;
+            float: left;
+            margin-bottom: 60px;
+          }
+          .int_name {
+            width: 118px;
+            height: 22px;
+            border: solid 1px #53b1dc;
+            line-height: 20px;
+            padding-left: 6px;
+          }
         }
-      }
-      .info{
-        color: #4ac0f6;
-        background-color: transparent;
-        text-decoration: underline;
+        .saveTeamName {
+          display: block;
+          float: left;
+          width: 50px;
+          height: 24px;
+          background-color: #169bd8;
+          text-align: center;
+          line-height: 24px;
+          font-size: 16px;
+          color: #ffffff;
+          margin-left: 34px;
+        }
       }
     }
   }
-}
 </style>
 
